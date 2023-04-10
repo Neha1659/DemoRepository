6496ee7a179533490e19a166e17b6f36fd5f32ed
“Neha1659” <nehareddyanthireddy@gmail.com>
2023-04-10 15:13:44 +0530

    [ADD:]check employee is present or not

--- employee.sh
+++ employee.sh
@@ -1,1 +1,11 @@
 echo "Welcome to employee computation program"
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
+
